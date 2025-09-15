e2c6056dca26dce716f74c166342d336502f60ea
Ryan Cheneka <1rychv@users.noreply.github.com>
2025-09-16 01:44:35 +0800

    fix(theme): wrap app with Restyle ThemeProvider and provide custom theme; alias Nav ThemeProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider as NavThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ThemeProvider as RestyleThemeProvider } from '@shopify/restyle';
+import { theme } from '@/ui/theme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,12 +15,14 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <NavThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RestyleThemeProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </RestyleThemeProvider>
+    </NavThemeProvider>
   );
 }
